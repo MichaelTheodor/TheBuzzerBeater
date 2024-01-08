15939b82fe2f0b9b8d9029146cae35ed79ef1e36
MichaelTheodor <michael.theodoropoulos@gmail.com>
2024-01-09 00:11:06 +0200

    Creating Category class,ApplicationDbContext class and database with the connectionstring.

---
+++ E-CommerceShop_TheBuzzerBeater/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using E_CommerceShop_TheBuzzerBeater.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace E_CommerceShop_TheBuzzerBeater.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ E-CommerceShop_TheBuzzerBeater/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_CommerceShop_TheBuzzerBeater.Models
+{
+    public class Category
+    {
+        [Key] 
+        public int CategoryId { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+    }
+}
