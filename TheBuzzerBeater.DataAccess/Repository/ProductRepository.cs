708b318d6b8668fe6012004fd2d09071899bcd52
MichaelTheodor <michael.theodoropoulos@gmail.com>
2024-01-12 11:54:15 +0200

    Creating IProductRepository,ProductRepository and fixing IUnitOfWork and UnitOfWork

---
+++ TheBuzzerBeater.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using TheBuzzerBeater.DataAccess.Data;
+using TheBuzzerBeater.DataAccess.Repository.IRepository;
+using TheBuzzerBeater.Models;
+
+namespace TheBuzzerBeater.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        
+
+        public void Update(Product obj)
+        {
+            _db.Products.Update(obj);
+        }
+    }
+}
