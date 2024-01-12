5092e6569d4537ca257246ee3e8c21fb5c345979
MichaelTheodor <michael.theodoropoulos@gmail.com>
2024-01-12 13:02:37 +0200

    Create Product Controller and Views(CRUD)

---
+++ E-CommerceShop_TheBuzzerBeater/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,94 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using TheBuzzerBeater.DataAccess.Data;
+using TheBuzzerBeater.DataAccess.Repository.IRepository;
+using TheBuzzerBeater.Models;
+
+namespace TheBuzzerBeater.Web.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll().ToList();
+            return View(objProductList);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Add(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product created succesfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Edit(int? productId)
+        {
+            if (productId == null || productId == 0)
+            {
+                return NotFound();
+            }
+            Product? productFromDb = _unitOfWork.Product.Get(u => u.ProductId == productId);
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+        }
+        [HttpPost]
+        public IActionResult Edit(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product updated succesfully";
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int? productId)
+        {
+            if (productId == null || productId == 0)
+            {
+                return NotFound();
+            }
+            Product? productFromDb = _unitOfWork.Product.Get(u => u.ProductId == productId);
+            if (productFromDb == null)
+            {
+                return NotFound();
+            }
+            return View(productFromDb);
+        }
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeletePOST(int? productId)
+        {
+            Product? obj = _unitOfWork.Product.Get(u => u.ProductId == productId);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _unitOfWork.Product.Remove(obj);
+            _unitOfWork.Save();
+            TempData["success"] = "Product deleted succesfully";
+            return RedirectToAction("Index");
+
+        }
+    }
+}
