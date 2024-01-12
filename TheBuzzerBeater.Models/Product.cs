23100a064f84fdbab996b0aa58ee5fd065af9211
MichaelTheodor <michael.theodoropoulos@gmail.com>
2024-01-12 11:20:13 +0200

    Create Product class and class properties

---
+++ TheBuzzerBeater.Models/Product.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheBuzzerBeater.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        [DisplayName("Product Name")]
+        public string Name { get; set; }
+
+        [StringLength(1000)]
+        public string Description { get; set; }
+
+        [Required]
+        [DataType(DataType.Currency)]
+        [Range(1, 2000)]
+        public double Price { get; set; }
+
+
+
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        
+        public Category Category { get; set; }
+        
+        public string ImageUrl { get; set; }
+    }
+}
+
