663be8305616eb19defaee5e059f8f076aa1f5ce
MichaelTheodor <michael.theodoropoulos@gmail.com>
2024-01-14 23:26:25 +0200

    Delete product and sweetAlerts.

--- E-CommerceShop_TheBuzzerBeater/Areas/Admin/Controllers/ProductController.cs
+++ E-CommerceShop_TheBuzzerBeater/Areas/Admin/Controllers/ProductController.cs
@@ -108,33 +108,7 @@ namespace TheBuzzerBeater.Web.Areas.Admin.Controllers
         }
 
         
-        public IActionResult Delete(int? productId)
-        {
-            if (productId == null || productId == 0)
-            {
-                return NotFound();
-            }
-            Product? productFromDb = _unitOfWork.Product.Get(u => u.ProductId == productId);
-            if (productFromDb == null)
-            {
-                return NotFound();
-            }
-            return View(productFromDb);
-        }
-        [HttpPost, ActionName("Delete")]
-        public IActionResult DeletePOST(int? productId)
-        {
-            Product? obj = _unitOfWork.Product.Get(u => u.ProductId == productId);
-            if (obj == null)
-            {
-                return NotFound();
-            }
-            _unitOfWork.Product.Remove(obj);
-            _unitOfWork.Save();
-            TempData["success"] = "Product deleted succesfully";
-            return RedirectToAction("Index");
-
-        }
+        
 
         #region API CALLS
 
@@ -144,6 +118,30 @@ namespace TheBuzzerBeater.Web.Areas.Admin.Controllers
             List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
 
             return Json(new { data = objProductList });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? productId)
+        {
+            
+            var productToBeDeleted = _unitOfWork.Product.Get(u => u.ProductId == productId);
+            if(productToBeDeleted == null) 
+            {
+                return Json(new { success = false,message = "Error while deleting"});
+            }
+
+            var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath,
+                                productToBeDeleted.ImageUrl.TrimStart('\\'));
+
+            if (System.IO.File.Exists(oldImagePath))
+            {
+                System.IO.File.Delete(oldImagePath);
+            }
+
+            _unitOfWork.Product.Remove(productToBeDeleted); 
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Deleted Succesfully" });
         }
 
         #endregion
--- E-CommerceShop_TheBuzzerBeater/wwwroot/js/product.js
+++ E-CommerceShop_TheBuzzerBeater/wwwroot/js/product.js
@@ -1,4 +1,4 @@
-
+var dataTable;
 
 $(function () {
     loadDataTable();
@@ -18,7 +18,7 @@ function loadDataTable() {
                     return `<div class="w-75 btn-group" role="group">
                     <a href="/admin/product/upsert?productId=${data}" class="btn btn-primary mx-2">
                             <i class="bi bi-pencil-square"></i> Edit</a>
-                    <a href="" class="btn btn-danger mx-2">
+                    <a onClick=Delete("/admin/product/delete/?productId=${data}") class="btn btn-primary mx-2">
                             <i class="bi bi-trash3-fill"></i> Delete</a>
                     </div>`
                 },
@@ -27,8 +27,42 @@ function loadDataTable() {
         ],
         "initComplete": function (settings, json) {
             // Apply row styling here
-            $('#tblData tbody tr').addClass('table-dark');
-            $('#tblData tbody td').css('font-size', '20px');
+            applyTableStyling();
         }
     });
 }
+
+// Function to apply styling to DataTable rows and cells
+function applyTableStyling() {
+    $('#tblData tbody tr').addClass('table-dark');
+    $('#tblData tbody td').css('font-size', '20px');
+}
+
+// Function to handle delete action
+function Delete(url) {
+    Swal.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'Delete',
+                success: function (data) {
+                    dataTable.ajax.reload(function () {
+                        // Callback function to be executed after DataTable is reloaded
+                        applyTableStyling();
+                        toastr.success(data.message);
+                    });   
+                }
+            });
+        }
+    });
+}
+
+
