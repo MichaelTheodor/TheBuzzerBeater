950ee12e82283898117c03bdaf10007c4bac14a8
MichaelTheodor <michael.theodoropoulos@gmail.com>
2024-01-17 04:04:53 +0200

    Create ShoppingCart class.Modify ApplicationDbContext.Add migration for Shopping Cart and update db.

--- TheBuzzerBeater.DataAccess/Data/ApplicationDbContext.cs
+++ TheBuzzerBeater.DataAccess/Data/ApplicationDbContext.cs
@@ -20,6 +20,7 @@ namespace TheBuzzerBeater.DataAccess.Data
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<Product> Products { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
