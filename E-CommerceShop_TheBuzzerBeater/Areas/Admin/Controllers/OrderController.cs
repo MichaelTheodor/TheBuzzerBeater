411883eebad92d796da322f897c1b4f8af257638
MichaelTheodor <michael.theodoropoulos@gmail.com>
2024-01-21 02:01:30 +0200

    Create Order VM and Order Controller.

---
+++ E-CommerceShop_TheBuzzerBeater/Areas/Admin/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using System.Numerics;
+using TheBuzzerBeater.DataAccess.Repository.IRepository;
+using TheBuzzerBeater.Models;
+
+namespace TheBuzzerBeater.Web.Areas.Admin.Controllers
+{
+    public class OrderController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public OrderController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<OrderHeader> objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+
+            return Json(new { data = objOrderHeaders });
+        }
+
+    
+
+        #endregion
+    }
+}
